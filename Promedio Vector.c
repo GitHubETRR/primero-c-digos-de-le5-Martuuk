13c020871f66bc7e9006ff2df8b034f3b75486c1
Martuuk <mkaciuk@etrr.edu.ar>
2025-10-24 13:10:39 -0300

    Update Promedio Vector.c

--- Promedio Vector.c
+++ Promedio Vector.c
@@ -1,37 +1,47 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#define TAM 5
+#define INFERIOR 1
+#define SUPERIOR 10
 
-int SumaVector(int vector[]);
-float PromedioVector(int vector[]);
+int SumaVector(int vector[], int tam);
+int Random (int inf, int sup);
+void VectorRandom (int vector [], int tam, int inf, int sup);
+float PromedioVector (int suma, int tam);
 
 int main() {
-    int vector[5] = {7, 9, 4, 11, 10};
-    int suma;
-    float promedio;
-
-    suma = SumaVector(vector);
-    printf("La suma de los elementos del vector es: %d\n", suma);
-
-    promedio = PromedioVector(vector);
-    printf("El promedio de los elementos del vector es: %f\n", promedio);
-
-    return 0;
+    int suma, vector[TAM];
+    float prom;
+    srand(time(NULL));
+    VectorRandom(vector, TAM, INFERIOR, SUPERIOR);
+	suma = SumaVector(vector, TAM);
+	printf("La suma de los elementos del vector es: %d\n", suma);
+	for (int i = 0; i < TAM; i++){
+	    printf ("el elemento %d vale %d \n", (i+1), vector[i]);
+	}
+	prom = PromedioVector(suma, TAM);
+	printf("El promedio de los elementos del vector es: %2.1f\n", prom);
+	return 0;
 }
 
-int SumaVector(int vector[]) {
-    int suma = 0;
-    for (int i = 0; i < 5; i++) {
-        suma = suma + vector[i];
+int SumaVector(int vector[], int tam) {
+	int suma = 0;
+	for (int i = 0; i < tam; i++) {
+		suma = suma + vector[i];
+	}
+	return suma;
+}
+int Random (int inf, int sup) {
+    return (inf + rand() % (sup-inf+1));
+}
+
+void VectorRandom (int vector [], int tam, int inf, int sup){
+    for (int i = 0; i < tam; i++) {
+        vector[i] = Random(inf, sup);
     }
-    return suma;
 }
 
-float PromedioVector(int vector[]) {
-    int suma = 0;
-    float prom = 0;
-    for (int i = 0; i < 5; i++) {
-        suma = suma + vector[i];
-        prom = suma/5;
-    }
-    return prom;  
+float PromedioVector(int suma, int tam) {
+	return ((float)suma/tam);
 }
-
