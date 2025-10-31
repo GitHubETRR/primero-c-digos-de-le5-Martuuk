4c37b1c0e9ee226624aab32a745918369d03962c
Martuuk <mkaciuk@etrr.edu.ar>
2025-10-31 12:52:27 -0300

    Update Producto escalar Vector.c

--- Producto escalar Vector.c
+++ Producto escalar Vector.c
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-#define TAM 3
+#define TAM 5
 #define INFERIOR 0
 #define SUPERIOR 4
-int CalculoEscalar (void);
+int CalculoEscalar (int vector1[], int vector2[],int tam);
+int Random (int inf, int sup);
+void VectorRandom (int vector1 [], int vector2[], int tam, int inf, int sup);
+
 int main() {
-	CalculoEscalar();
+	int vector1[TAM];
+	int vector2 [TAM];
+	srand(time(NULL));
+	VectorRandom (vector1, vector2, TAM, INFERIOR, SUPERIOR);
+
+	int productoEscalar= CalculoEscalar(vector1, vector2, TAM);
+
+	printf ("el producto escalar es %d\n", productoEscalar);
+
+	for (int i = 0; i < TAM; i++) {
+		printf ("par %d° %d_%d\n",(i+1), vector1[i],vector2[i] );
+	}
 	return 0;
 }
 
-int CalculoEscalar (void) {
-	int ProductoEscalar=0;
-	int vector1[TAM];
-	int vector2 [TAM];
-
-	//RANDOMIZA LOS VALORES DEL VECTOR
-	srand(time(NULL));
-	for (int i = 0; i < TAM; i++) {
-		vector1[i] = INFERIOR + rand() % (SUPERIOR-INFERIOR+1);
-		vector2[i] = INFERIOR + rand() % (SUPERIOR-INFERIOR+1);
+int CalculoEscalar (int vector1[], int vector2[],int tam) {
+	int productoEscalar=0;
+	for (int i = 0; i < tam; i++) {
+		productoEscalar = productoEscalar + vector1[i]*vector2[i];
 	}
+	return productoEscalar;
+}
 
-	//calcula producto escalar
-	for (int i = 0; i < TAM; i++) {
-		ProductoEscalar = ProductoEscalar + vector1[i]*vector2[i];
-	}
 
-	printf ("%d   ", ProductoEscalar);
-	//escribe cada par de los valores de cada vector
-	for (int i = 0; i < TAM; i++) {
-		printf ("%d_%d ",vector1[i],vector2[i] );
+void VectorRandom (int vector1 [], int vector2[], int tam, int inf, int sup) {
+	for (int i = 0; i < tam; i++) {
+		vector1[i] = Random(inf, sup);
+		vector2[i] = Random(inf, sup);
 	}
+}
+
+int Random (int inf, int sup) {
+	return (inf + rand() % (sup-inf+1));
 }
