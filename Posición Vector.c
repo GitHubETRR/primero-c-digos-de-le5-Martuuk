46b7e02cee220fa15c925d58227869286976c1f2
Martuuk <mkaciuk@etrr.edu.ar>
2025-10-31 12:51:41 -0300

    Update Posición Vector.c

--- Posición Vector.c
+++ Posición Vector.c
@@ -4,36 +4,80 @@
 #define TAM 100
 #define INFERIOR 10
 #define SUPERIOR 40
-int repeticion_vector (void);
-int main(){
-    repeticion_vector();
-    return 0;
+
+int Random (int inf, int sup);
+void VectorRandom (int vector [], int tam, int inf, int sup);
+int RepeticionMayor (int vector[], int tam, int inf);
+int DetectorMayor (int vector[], int tam, int inf);
+int DetectorMenor (int vector[], int tam, int sup);
+void PosicionMayor (int vector[], int tam, int mayor);
+
+int main() {
+	int vector[TAM];
+	srand(time(NULL));
+	VectorRandom(vector, TAM, INFERIOR, SUPERIOR);
+
+	int repeticion = RepeticionMayor(vector, TAM, INFERIOR);
+	printf ("el mayor se repite %d veces \n", repeticion);
+
+	int mayor = DetectorMayor (vector, TAM, INFERIOR);
+	int menor = DetectorMenor (vector, TAM, SUPERIOR);
+	printf("El menor es %d y el mayor es %d\n", menor, mayor);
+
+	PosicionMayor (vector,TAM, mayor);
+
+	for (int i = 0; i < TAM; i++) {
+		printf ("%d ",vector[i]);
+	}
+	return 0;
 }
 
-int repeticion_vector (void){
-    int mayor=0, repeticion=0, menor=40;
-    int vector[TAM];
-    srand(time(NULL));
-    //RANDOMIZA LOS VALORES DEL VECTOR
-    for (int i = 0; i < TAM; i++) {
-        vector[i] = INFERIOR + rand() % (SUPERIOR-INFERIOR+1);
-    }
-    //Busca el mayor valor
-    for (int i = 0; i < TAM; i++){
-        if (vector[i]>mayor) {
-            mayor=vector[i];
-        }
-    }
-    //Busca el menor valor
-     for (int i = 0; i < TAM; i++){
-        if (vector[i]<menor) {
-            menor=vector[i];
-        }
-    }
-    printf ("el mayor es %d y el menor es %d \n", mayor, menor);
-    //ubica y escribe todas las posiciones del mayor
-    for (int i = 0; i < TAM; i++){
-        if (vector[i]==mayor) 
-        printf ("el mayor se encuentra en la posicion %d \n",i );
-    }
+
+int Random (int inf, int sup) {
+	return (inf + rand() % (sup-inf+1));
+}
+
+void VectorRandom (int vector [], int tam, int inf, int sup) {
+	for (int i = 0; i < tam; i++) {
+		vector[i] = Random(inf, sup);
+	}
+}
+
+int DetectorMayor (int vector[], int tam, int inf) {
+	int mayor=inf;
+	for (int i = 0; i < tam; i++) {
+		if (vector[i]>mayor) {
+			mayor=vector[i];
+		}
+	}
+	return mayor;
+}
+
+int DetectorMenor (int vector[], int tam, int sup) {
+	int menor=sup;
+	for (int i = 0; i < tam; i++) {
+		if (vector[i]<menor) {
+			menor=vector[i];
+		}
+	}
+	return menor;
+}
+
+int RepeticionMayor (int vector[], int tam, int inf) {
+	int repeticion=0;
+	int mayor= DetectorMayor(vector, tam, inf);
+
+	for (int i = 0; i < TAM; i++) {
+		if (vector[i]==mayor) {
+			repeticion++;
+		}
+	}
+	return repeticion;
+}
+
+void PosicionMayor (int vector[], int tam, int mayor) {
+	for (int i = 0; i < tam; i++) {
+		if (vector[i]==mayor)
+			printf ("el mayor se encuentra en la posicion %d \n",i );
+	}
 }
